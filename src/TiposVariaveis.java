244bf841e4db377a197f27cddd178c773cfe65df
Heitor Bailke de Bem <heitorbailkedev@hotmail.com>
2025-03-15 12:21:20 -0300

    Aprendendo novas variaveis

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,5 +1,14 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        double salarioMinimo = 2500;
+        byte = idade 123;
+        short ano = 2021;
+        int cep = 21070333 // se começar com zero, talvez tem que ser de outro tipo (string)
+        long cpf = 17838579783L; // tem que terminar com "L"// se começar com zerp, talvez tem que ser de outro tipo(string)
+        float pi = 3.14F; // tem que ser finalizada com F
+        double salario = 1275.33;
+
+        final double valor_De_pi = 3.14; // "final" é para garantir que a variavel nao pode ser mudada
+        
     }
 }
